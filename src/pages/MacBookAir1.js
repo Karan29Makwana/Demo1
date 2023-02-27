41a64080732191bdbc63a23951c4d0c7edfff985
Karan29Makwana <103408415+Karan29Makwana@users.noreply.github.com>
2023-02-27 15:25:34 +0530

    Add files via upload

---
+++ src/pages/MacBookAir1.js
@@ -0,0 +1,119 @@
+import "./MacBookAir1.css";
+
+const MacBookAir1 = () => {
+  return (
+    <div className="macbook-air-1">
+      <img
+        className="macbook-air-1-child"
+        alt=""
+        src="../manimg.png"
+      />
+      <b className="diane-cooper">Diane Cooper</b>
+      <div className="div">15</div>
+      <div className="div1">02</div>
+      <div className="female">Male</div>
+      <div className="jl-diponegoro-no">JL. Diponegoro No. 21</div>
+      <div className="active-member">Active Member</div>
+      <div className="feb-24th-1997">Feb 24th, 1997</div>
+      <div className="cilacap">Cilacap</div>
+      <div className="feb-24th-19971">Feb 24th, 1997</div>
+      <div className="div2">(239) 555 -0108</div>
+      <div className="div3">655849</div>
+      <div className="dianecooperexamplecom">diane.cooper@example.com</div>
+      <div className="past">Past</div>
+      <div className="upcoming">Upcoming</div>
+      <div className="gender">Gender</div>
+      <div className="street-address">Street Address</div>
+      <div className="member-status">Member Status</div>
+      <div className="registered-date">Registered Date</div>
+      <div className="city">City</div>
+      <div className="birthday">Birthday</div>
+      <div className="phone-number">Phone Number</div>
+      <div className="zip-code">Zip Code</div>
+      <img className="macbook-air-1-item" alt="" src="../vector-735.svg" />
+      <img className="macbook-air-1-inner" alt="" src="../vector-739.svg" />
+      <img className="vector-icon" alt="" src="../vector-739.svg" />
+      <img className="macbook-air-1-child1" alt="" src="../vector-739.svg" />
+      <img className="macbook-air-1-child2" alt="" src="../vector-739.svg" />
+      <img className="macbook-air-1-child3" alt="" src="../vector-739.svg" />
+      <img className="macbook-air-1-child4" alt="" src="../vector-739.svg" />
+      <img className="macbook-air-1-child5" alt="" src="../vector-739.svg" />
+      <img className="macbook-air-1-child6" alt="" src="../vector-739.svg" />
+      <button className="send-message-wrapper">
+        <div className="send-message">Send Message</div>
+      </button>
+      <div className="frame-parent">
+        <div className="upcoming-appointments-wrapper">
+          <div className="upcoming-appointments">Upcoming Appointments</div>
+        </div>
+        <div className="past-appointments-wrapper">
+          <div className="past-appointments">Past Appointments</div>
+        </div>
+        <div className="past-appointments-wrapper">
+          <div className="past-appointments">Medical Records</div>
+        </div>
+      </div>
+      <div className="rectangle-div" />
+      <div className="root-canal-treatment">Root Canal Treatment</div>
+      <div className="show-previous-treatments-wrapper">
+        <div className="past-appointments">Show Previous Treatments</div>
+      </div>
+      <div className="macbook-air-1-child7" />
+      <div className="div4">09.00 - 10.00</div>
+      <b className="nov-19">26 Nov ‘19</b>
+      <div className="open-access">Open Access</div>
+      <div className="drg-adam-h">Drg. Adam H.</div>
+      <div className="jessicamila">Jessicamila</div>
+      <div className="treatment">Treatment</div>
+      <div className="dentist">Dentist</div>
+      <div className="nurse">Nurse</div>
+      <div className="note">Note</div>
+      <img className="macbook-air-1-child8" alt="" src="../vector-749.svg" />
+      <img className="macbook-air-1-child9" alt="" src="../vector-750.svg" />
+      <img className="note-2-icon" alt="" src="../note-2.svg" />
+      <div className="macbook-air-1-child10" />
+      <div className="div5">09.00 - 10.00</div>
+      <b className="nov-191">12 Nov ‘19</b>
+      <div className="root-canal">Root Canal</div>
+      <div className="drg-adam-h1">Drg. Adam H.</div>
+      <div className="jessicamila1">Jessicamila</div>
+      <div className="treatment1">Treatment</div>
+      <div className="dentist1">Dentist</div>
+      <div className="nurse1">Nurse</div>
+      <div className="note1">Note</div>
+      <img className="note-3-icon" alt="" src="../note-2.svg" />
+      <div className="files-documents">Files / Documents</div>
+      <div className="check-up-resultspdf">Check Up Results.pdf</div>
+      <img className="note-4-icon" alt="" src="../note-4.svg" />
+      <div className="check-up-resultspdf1">Check Up Results.pdf</div>
+      <img className="note-5-icon" alt="" src="../note-4.svg" />
+      <div className="medical-prescriptionpdf">Medical Prescription.pdf</div>
+      <img className="note-6-icon" alt="" src="../note-4.svg" />
+      <div className="dental-x-ray-resultpdf">Dental X-Ray Result.pdf</div>
+      <img className="note-1-icon" alt="" src="../note-4.svg" />
+      <div className="rectangle-parent">
+        <div className="group-child" />
+        <div className="group-item" />
+        <div className="new-patient-folder-container">
+          <p className="new">{`NEW `}</p>
+          <p className="new">PATIENT</p>
+          <p className="new">{`FOLDER `}</p>
+          <p className="new">{`UPLOAD `}</p>
+          <p className="new">{`REPORT `}</p>
+          <p className="new">SETTING</p>
+          <p className="new">LOGOUT</p>
+        </div>
+        <img className="mask-group-icon" alt="" src="../mask-group.svg" />
+        <div className="div6">+</div>
+        <img className="image-897-icon" alt="" src="../image-897@2x.png" />
+        <img className="image-898-icon" alt="" src="../image-898@2x.png" />
+        <img className="image-899-icon" alt="" src="../image-899@2x.png" />
+        <img className="image-900-icon" alt="" src="../image-900@2x.png" />
+        <img className="image-901-icon" alt="" src="../image-901@2x.png" />
+        <img className="image-902-icon" alt="" src="../image-902@2x.png" />
+      </div>
+    </div>
+  );
+};
+
+export default MacBookAir1;
